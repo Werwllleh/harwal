4b957a18f2916a3818f4694487b35cfcab3b61d1
Alexey A <allvxm4@gmail.com>
2026-01-19 18:17:22 +0300

    main page st3

--- src/components/button-info/button-info.js
+++ src/components/button-info/button-info.js
@@ -7,20 +7,63 @@ if (buttonInfoElements.length) {
     if (!actionButton || !description) return;
 
     actionButton.addEventListener('click', (e) => {
+      e.stopPropagation();
 
-      if (element.classList.contains('active')) {
+      const isActive = element.classList.contains('active');
+
+      if (isActive) {
         element.classList.remove('active');
+
+        setTimeout(() => {
+          description.style.left = ''
+          description.style.right = ''
+          description.style.transformOrigin = ''
+          description.style.display = ''
+        }, 150)
       } else {
+        document.querySelectorAll('.button-info.active').forEach(i => {
+          i.classList.remove('active');
+        })
+
+        description.style.left = ''
+        description.style.right = ''
+        description.style.transformOrigin = ''
+        description.style.display = 'block';
+
+        const rect = description.getBoundingClientRect();
+        const viewportWidth = window.innerWidth;
+
+        if (rect.right + 200 > viewportWidth) {
+          description.style.left = '';
+          description.style.right = '3rem';
+          description.style.transformOrigin = 'right top';
+        } else {
+          description.style.left = '3rem';
+          description.style.right = '';
+          description.style.transformOrigin = 'left top';
+        }
+
         element.classList.add('active');
       }
-    })
-
-    document.addEventListener('click', (e) => {
-      buttonInfoElements.forEach(i => {
-        if (!i.contains(e.target)) {
-          i.classList.remove('active');
-        }
-      });
     });
-  })
+  });
+
+  document.addEventListener('click', (e) => {
+    buttonInfoElements.forEach(i => {
+      const description = i.querySelector('.button-info__description');
+
+      if (!i.contains(e.target)) {
+        i.classList.remove('active');
+
+        setTimeout(() => {
+          description.style.left = ''
+          description.style.right = ''
+          description.style.transformOrigin = ''
+          description.style.display = ''
+        }, 150)
+
+      }
+
+    });
+  });
 }
