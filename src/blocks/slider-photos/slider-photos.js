f3f8dd07c6e20987a4a76d9d86a6492f1fbf2a09
Alexey A <allvxm4@gmail.com>
2026-01-25 15:53:32 +0300

    page-production st2

---
+++ src/blocks/slider-photos/slider-photos.js
@@ -0,0 +1,28 @@
+const sliderPhotosBlocks = document.querySelectorAll('.slider-photos');
+if (sliderPhotosBlocks.length) {
+  sliderPhotosBlocks.forEach(block => {
+    const swiper = block.querySelector('.swiper');
+
+    if (!swiper) return;
+
+    new Swiper(swiper, {
+      loop: true,
+      spaceBetween: 30,
+      slidesPerView: "auto",
+      centeredSlides: true,
+    });
+
+    lightGallery(swiper, {
+      plugins: [lgThumbnail],
+      licenseKey: '0000-0000-0000-0000', // тестовый ключ
+      speed: 300,
+      download: false, // скрыть кнопку загрузки
+      animateThumb: true,
+      // zoomFromOrigin: false,
+      // allowMediaOverlap: true,
+      toggleThumb: true,
+      thumbnail: true,
+      selector: '.swiper-slide a'
+    });
+  })
+}
