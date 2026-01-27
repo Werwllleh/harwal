1bb2e5622956955e13317e8907f76c482058f2e4
Alexey A <allvxm4@gmail.com>
2026-01-27 20:02:12 +0300

    page service-detail st1

---
+++ src/blocks/subsections/subsections.js
@@ -0,0 +1,20 @@
+const subsectionsBlocks = document.querySelectorAll('.subsections');
+if (subsectionsBlocks.length) {
+  subsectionsBlocks.forEach(subsectionBlock => {
+    const buttons = subsectionBlock.querySelectorAll('.subsections__list li button');
+    if (!buttons.length) return;
+
+    buttons.forEach(button => {
+      button.addEventListener('click', (e) => {
+        e.preventDefault();
+        const sectionName = button.dataset.subsection;
+        const section = document.querySelector(`[data-section="${sectionName}"`);
+
+        if (!sectionName || !section) return;
+
+        section.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+      })
+    })
+
+  })
+}
